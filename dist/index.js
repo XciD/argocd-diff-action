86f78f8cfc0b341a953fa3e1c6a21f17286d0adb
coyotte508 <coyotte508@protonmail.com>
2025-10-16 14:14:21 +0200

    edit dist manually =/

--- dist/index.js
+++ dist/index.js
@@ -1795,7 +1795,7 @@ ${diff}
             : ''}
 ---
 `);
-        const output = scrubSecrets(`
+        let output = scrubSecrets(`
 ## ArgoCD Diff for commit [\`${shortCommitSha}\`](${commitLink})
 _Updated at ${new Date().toLocaleString('en-US', { timeZone: 'Europe/Paris' })} CEST_
   ${diffOutput.join('\n')}
@@ -1806,6 +1806,14 @@ _Updated at ${new Date().toLocaleString('en-US', { timeZone: 'Europe/Paris' })} 
 | ⚠️      | The app is out-of-sync in ArgoCD, and the diffs you see include those changes plus any from this PR. |
 | 🛑     | There was an error generating the ArgoCD diffs due to changes in this PR. |
 `);
+
+        if (output.length >= 65535) {
+            output = `${output.slice(
+                0,
+                65000
+            )}...\n\n**Warning: The comment is too long and has been truncated.**`;
+        }
+
         const commentsResponse = yield octokit.rest.issues.listComments({
             issue_number: github.context.issue.number,
             owner,
